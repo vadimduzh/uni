3466b1e1f2737562783a0fe7a18d859e98c5723e
vadimduzh <Vadim.duzh@gmail.com>
2024-09-22 11:41:19 +0300

    added

---
+++ 2024_09_18/gcm.py
@@ -0,0 +1,11 @@
+def gcd(a, b):
+    while b != 0 and a != 0:
+        b = b % a
+        b, a = a, b
+    return b
+
+
+n_1, n_2 = [int(s) for s in input("").split()]
+
+res = int((n_1 * n_2) / gcd(n_1, n_2))
+print(res)
