6fa0c11964cf22d79a442a2f345b1660bf098288
vadimduzh <Vadim.duzh@gmail.com>
2024-09-23 20:35:33 +0300

    added

--- 2024_09_18/find_ones.py
+++ 2024_09_18/find_ones.py
@@ -1,12 +1,10 @@
-def count_ones(x):
-    count = 0
-    while x > 0:
-        if x % 2 != 0:
-            count += 1
-        x = x // 2
-    return count
-
-
 n = int(input(""))
-value = count_ones(n)
-print(value)
+
+count = 0
+while n > 0:
+    if n % 2 != 0:
+        count += 1
+    n = n // 2
+
+print(count)
+
